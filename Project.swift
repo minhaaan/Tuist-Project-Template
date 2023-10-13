fc255eb91d0876fbe9a34aea7751ed73578eed3f
최민한 <chmh0411@gmail.com>
2023-10-14 03:31:49 +0900

    Plugin import하는 부분 제거

--- Project.swift
+++ Project.swift
@@ -1,27 +1,5 @@
 import ProjectDescription
 import ProjectDescriptionHelpers
-import MyPlugin
-
-/*
-                +-------------+
-                |             |
-                |     App     | Contains TuistProjectTemplate App target and TuistProjectTemplate unit-test target
-                |             |
-         +------+-------------+-------+
-         |         depends on         |
-         |                            |
- +----v-----+                   +-----v-----+
- |          |                   |           |
- |   Kit    |                   |     UI    |   Two independent frameworks to share code and start modularising your app
- |          |                   |           |
- +----------+                   +-----------+
-
- */
-
-// MARK: - Project
-
-// Local plugin loaded
-let localHelper = LocalHelper(name: "MyPlugin")
 
 let dependencies: [TargetDependency] = [
 ]
