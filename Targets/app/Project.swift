9f40198f1a28ade9cd467cba12d6a2c76677fe7a
최민한 <chmh0411@gmail.com>
2023-10-14 04:09:50 +0900

    App Project Setting

--- Targets/app/Project.swift
+++ Targets/app/Project.swift
@@ -1,13 +1,56 @@
 import ProjectDescription
 import ProjectDescriptionHelpers
 
-let dependencies: [TargetDependency] = [
+let baseSetting: [String: SettingValue] = [
+  "DEVELOPMENT_TEAM": "7T8JF3V3RR"
 ]
+
+let appTarget = Target(
+  name: Setting.appName,
+  platform: .iOS,
+  product: .app,
+  productName: Setting.appName,
+  bundleId: Setting.bundleId,
+  deploymentTarget: Setting.deploymentTarget,
+  infoPlist: .extendingDefault(with: [
+    "CFBundleShortVersionString": "1.0",
+    "CFBundleVersion": "1",
+    "UIMainStoryboardFile": "",
+    "UILaunchStoryboardName": "LaunchScreen",
+  ]),
+  sources: ["Sources/**"],
+  resources: ["Resources/**"],
+  dependencies: [
+  ],
+  settings: .settings(
+    base: baseSetting,
+    configurations: [.debug(name: "Debug"), .release(name: "Release")],
+    defaultSettings: .recommended
+  )
+)
+
+let testTarget = Target(
+  name: "\(Setting.appName)Tests",
+  platform: .iOS,
+  product: .unitTests,
+  bundleId: "\(Setting.bundleId)Tests",
+  deploymentTarget: Setting.deploymentTarget,
+  infoPlist: .default,
+  sources: ["Tests/**"],
+  dependencies: [
+  ],
+  settings: .settings(
+    base: baseSetting, configurations: [], defaultSettings: .recommended
+  )
+)
 
 let project = Project(
   name: Setting.appName,
   organizationName: "com.minan.TuistProjectTemplate",
   packages: [],
   settings: nil,
-  targets: 
+  targets: [
+    appTarget,
+    testTarget
+  ]
 )
