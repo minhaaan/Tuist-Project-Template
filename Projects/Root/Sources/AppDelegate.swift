1a08a73b8d6b213ac646fe313562646f9271447e
최민한 <chmh0411@gmail.com>
2024-09-23 00:13:54 +0900

    chore: Root App Setting

---
+++ Projects/Root/Sources/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  AppStore
+//
+//  Created by 최민한 on 9/7/24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  public var window: UIWindow?
+
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+
+    let window = UIWindow(frame: UIScreen.main.bounds)
+    self.window = window
+    
+    window.makeKeyAndVisible()
+    window.rootViewController = UIViewController()
+
+    return true
+  }
+
+}
+
